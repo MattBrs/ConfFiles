f902d7abe9ee008a197596fa4658493a8ba712b6
MattBrs <matteobrusarosco@tutamail.com>
2024-03-07 17:45:31 +0100

    [Wezterm] Update conf with font and keybindings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,14 @@
 local term = require("wezterm")
+local act = term.action
 local config = {}
 
 config.color_scheme = 'Monokai Dimmed'
 config.window_background_opacity = 0.84
+config.window_frame = {
+    font_size = 13.0
+}
 
+config.font_size = 12.0
 
 config.keys = {
     -- This will create a new split and run your default program inside it
@@ -12,6 +17,10 @@ config.keys = {
         mods = 'CTRL|ALT',
         action = term.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
     },
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    { key = "LeftArrow",  mods = "OPT", action = term.action { SendString = "\x1bb" } },
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    { key = "RightArrow", mods = "OPT", action = term.action { SendString = "\x1bf" } },
 }
 
 return config
